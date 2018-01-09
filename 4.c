87aa3aa68cbb5f67699fc374d89a066c48d84695
deepaulaganathan <31499645+deepaulaganathan@users.noreply.github.com>
2018-01-09 20:08:48 +0530

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+char alphabet;
+printf("enter an alphabet");
+alphabet=getchar();
+if(is lower(alphabet))
+putchar(to upper(alphabet));
+else
+putchar(to lower(alphabet));
+}
+
