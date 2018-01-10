97b6eacbc1a445da31061e1c4c05cee8dc5d903d
deepaulaganathan <31499645+deepaulaganathan@users.noreply.github.com>
2018-01-10 16:24:20 +0530

    Create s.c

---
+++ s.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char c;
+scanf("%d",&c);
+if(c=(c=a||c=e||c=i||c=o||c=u)
+printf("enter the c value");
+else
+printf("enter the consonant");
+}
