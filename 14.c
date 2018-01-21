ccb501f4dbf7246c7090d25b5814b209c4b561f2
deepaulaganathan <31499645+deepaulaganathan@users.noreply.github.com>
2018-01-21 11:48:36 +0530

    Create 14.c

---
+++ 14.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a,b,i,rem;
+printf("enter the first number");
+scanf("%d",&a);
+printf("enter the final number");
+scanf("%d",&b);
+{
+for(i=a;i<=b;i++)
+{
+if(rem==0)
+{
+printf("the odd number b/w %d and %d \n a,b);
+printf("\n%d",i);
+}
