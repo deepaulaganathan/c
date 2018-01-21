dc6c11cd83e4cca9b334b834aedd2a9e8c40a7d6
deepaulaganathan <31499645+deepaulaganathan@users.noreply.github.com>
2018-01-21 12:12:42 +0530

    Create 15.c

---
+++ 15.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int a,b,i,rem;
+printf("enter the first number");
+scanf("%d",&a);
+printf("enter the final number");
+scanf("%d",&b);
+for(i=a;i<=b;i++)
+{
+rem=i%2;
+}
+if(rem==0)
+{
+printf("the even number between %d and %d \n: a,b");
+prinf("\n%d",i);
+}
+}
