6ae1d1ebee40e275a8509658a527a2c0d38a4916
deepaulaganathan <31499645+deepaulaganathan@users.noreply.github.com>
2018-01-21 10:29:56 +0530

    Create 11.c

---
+++ 11.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int base ,exponent;
+int result;
+  printf("enter the base and exponent");
+  scanf(#include<stdio.h>
+void m"%d",&base and exponend);
+  while
+  {
+  result=result base;
+  --exponent;
+  }
+  printf("rersult is %d",result);
+  return 0
+  }
