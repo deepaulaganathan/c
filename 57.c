5276c434b1772c7325de4faadb27d7f52628e72d
deepaulaganathan <31499645+deepaulaganathan@users.noreply.github.com>
2018-02-26 16:38:20 +0530

    Create 57.c

---
+++ 57.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,temp;
+printf("enter the value of a and b");
+scanf("%d%d",&a,&b);
+printf("before swapping a=%d b=%d ,a,b);
+temp=x;
+x=y;
+y=temp;
+printf("after swapping a=%d b=%d ,a,b);
+return 0;
+}
