25f2ebf62c48919104a39e8ce77b9456d0905d1c
deepaulaganathan <31499645+deepaulaganathan@users.noreply.github.com>
2018-02-28 16:05:03 +0530

    Create 58.c

---
+++ 58.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int n=16;
+ int m=32;
+ printf("value of n=%d m=%d before swapping",n,m);
+ i=i^k;
+ k=i^k;
+ i=i^k;
+ print("value of n=%d m=%d after swapping",n,m);
+ return 0;
+ }
