a4537ffb2df92c8d2167b242d3311560f753ab66
deepaulaganathan <31499645+deepaulaganathan@users.noreply.github.com>
2018-03-02 16:36:47 +0530

    Create 52.c

---
+++ 52.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+//#include<conio.h>
+int main()
+{
+	int a;
+	printf("enter the number");
+	scanf("%d",&a);
+	switch(a);
+	{              case 0
+              printf("zero");
+              break;
+              case 1
+              printf("one");
+              break;
+	}
+	return 0;
+}
+
